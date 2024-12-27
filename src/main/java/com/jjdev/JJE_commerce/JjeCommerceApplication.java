668a96642c3e3f94f694e5fbab4523454539cfa7
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-12-27 11:48:30 -0300

    Primeiro comit

---
+++ src/main/java/com/jjdev/JJE_commerce/JjeCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jjdev.JJE_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjeCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjeCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjdev/JJE_commerce/JjeCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjdev.JJE_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjeCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
