f744bceca5f8b61cb3a5ec88ad4dba0e1e02debe
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-12-28 18:08:24 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/jjdev/JJE_commerce/repositories/UserRepository.java
+++ src/main/java/com/jjdev/JJE_commerce/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.jjdev.JJE_commerce.repositories;
 
 import com.jjdev.JJE_commerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/jjdev/JJE_commerce/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jjdev.JJE_commerce.services;
+
+import com.jjdev.JJE_commerce.entities.User;
+import com.jjdev.JJE_commerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+}
